476bd2eae5d2b201fd4b85aa1c16c623748a7567
JimboMcWhoopin <135807836+JimboMcWhoopin@users.noreply.github.com>
2023-06-07 02:55:50 -0400

    Update Minecraft-Server-Setup.sh
    Imma be real, I need a version of this for 1.19.2 with forge 43.2.3, Im like pretty sure i did this wrong if so, my b

--- Minecraft-Server-Setup.sh
+++ Minecraft-Server-Setup.sh
@@ -3,9 +3,9 @@ sudo firewall-cmd --permanent --zone=public --add-port=25565/udp
 sudo firewall-cmd --reload
 sudo yum -y update && sudo yum upgrade
 sudo yum -y install jdk-20
-wget https://maven.minecraftforge.net/net/minecraftforge/forge/1.19.3-44.1.0/forge-1.19.3-44.1.0-installer.jar
-java -jar forge-1.19.3-44.1.0-installer.jar --install
-rm forge-1.19.3-44.1.0-installer.jar forge-1.19.3-44.1.0-installer.jar.log
+wget https://maven.minecraftforge.net/net/minecraftforge/forge/1.19.2-43.2.3/forge-1.19.2-43.2.3-installer.jar
+java -jar forge-1.19.2-43.2.3-installer.jar --install
+rm forge-1.19.3-44.1.0-installer.jar forge-1.19.2-43.2.3-installer.jar.log
 ./run.sh
 echo "eula=true" > eula.txt
 echo " -Xmx20G" > user_jvm_args.txt
